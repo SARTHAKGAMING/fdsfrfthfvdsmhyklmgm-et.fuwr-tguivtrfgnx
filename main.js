1f06d315ca03d243e0c1d3301a837e09c0527803
SARTHAKGAMING <74373223+SARTHAKGAMING@users.noreply.github.com>
2021-04-27 20:58:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+function preload() {
+    //
+    //
+}
+
+function setup() {
+
+    var canvas=createCanvas(640,400);
+
+    canvas.position(630,250);
+var video=createCapture(VIDEO);
+video.hide();
+var tintcolour="";
+}
+
+function draw() {
+
+
+    Image(video,0,0,640,400);
+
+    tint(tintcolour);
+
+
+}
+function take_snapshot(){
+    save("YOU_ARE_PRO.png");
+
+}
+function change_btn(){
+    tintcolour=document.getElementById("take123").value;
+}
